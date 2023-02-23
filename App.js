38ada6e82b3d93ebb0d1d79457db4e8890a86545
AudreyTsafack <101474311+AudreyTsafack@users.noreply.github.com>
2023-02-23 16:19:17 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-import{login} from "./login";
-import {register} from "./register";
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
-  return (
-    <div className="App">
-      <login />
-    </div>
-  );
+    const [currentForm, setCurrentForm] = useState('login');
+
+    const toggleForm = (formName) => {
+        setCurrentForm(formName);
+    }
+
+    return (
+        <div className="App">
+            {
+                currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+            }
+        </div>
+    );
 }
 
 export default App;
